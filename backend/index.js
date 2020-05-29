235c9b6b8bac8b62194d9593fe1215326b9fbab7
Raylph Evangelista <rsevangelista@ucdavis.edu>
2020-05-29 04:31:02 -0700

    Login is working, now need just need to redirect to playlist page and get user data

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,114 @@
-const express =  require('express')
+const express =  require('express');
+const bodyParser = require('body-parser');
+
+const passport = require("passport");
+const SpotifyStrategy = require('passport-spotify').Strategy;
+
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
 const path = require('path');
 
-const app = express()
+var appKey = '7f6407d2ff194a87a5236a464044ec4e';
+var appSecret = '040e7b594df34f578a39875342e941bf';
+
+passport.serializeUser(function(user, done) {
+	done(null, user);
+})
+
+passport.deserializeUser(function(obj, done) {
+	done(null, obj);
+});
+
+// Spotify strategy
+passport.use(
+	new SpotifyStrategy({
+		clientID: appKey,
+		clientSecret: appSecret,
+		callbackURL: 'http://localhost:8000/auth/spotify/callback',
+	},
+	function(accessToken, refreshToken, expires_in, profile, done) {
+		process.nextTick(function() {
+      return done(null, profile);
+    });
+  })
+);
+
+const app = express();
+console.log("set up pipeline");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+app.use(expressSession({
+  secret:'cookieCat',
+  maxAge: 6 * 60 * 60 * 1000,
+  resave: true,
+  saveUninitialized: false,
+  name: "tunein-session-cookie"
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
+// Commented this bit out because it wouldn't go to the auth page
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'../frontend/build/index.html'));
-});
+// app.get('*', (req,res) =>{
+//     res.sendFile(path.join(__dirname+'/../frontend/build/index.html'));
+// });
+
+app.get('../frontend/src/pages/*', requireUser, requireLogin, express.static('.'));
+
+app.get('/auth/spotify', passport.authenticate('spotify'));
+
+app.get('/auth/spotify/callback',
+  passport.authenticate('spotify',
+    { successRedirect: '/setcookie', failureRedirect: '/' }
+  )
+);
+
+app.get('/setcookie', requireUser,
+  function(req, res) {
+    res.cookie('songs-with-friends', new Date());
+    res.redirect('/');
+  }
+);
+
+// app.get('/login', function(req, res) {
+//   res.render('../frontend/src/pages/Login.js', { user: req.user });
+// });
+
+// Don't think we need this...
+// app.get('/auth/spotify/callback', 
+//   passport.authenticate('spotify'),
+//   function(req, res) {
+//     res.redirect('/');
+//   }
+// );
+
+
+// Don't need this yet
+// app.get('/auth/logout', (req, res) => {
+//   console.log("logging out");
+//   res.redirect('/');  
+// });
+
+function requireUser(req, res, next) {
+  if(!req.user) {
+    res.redirect('/');
+  } else {
+    next();
+  }
+};
+
+function requireLogin(req, res, next) {
+  if(!req.cookies['tunein-session-cookie']) {
+    res.redirect('/');
+  } else {
+    next();
+  }
+};
 
 const port = process.env.PORT || 8000;
 app.listen(port);
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -10,7 +10,9 @@ const Login = () => {
                 <h1 className="title">TuneIn (a Songs with Friends project)</h1>
             </div>
             <div className="row">
+              <a href="auth/spotify">
                 <button className="login">CONNECT TO SPOTIFY</button>
+              </a>
             </div>
         </div>
     )
